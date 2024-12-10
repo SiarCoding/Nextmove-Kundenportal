a2dc7a6be618a322c4cc60a8138790a09225fd79
Siar Alizadah <salizadah8@gmail.com>
2024-12-10 16:33:39 +0100

    master

--- app/server/index.ts
+++ app/server/index.ts
@@ -118,10 +118,9 @@ app.use((req, res, next) => {
     serveStatic(app);
   }
 
-  // ALWAYS serve the app on port 5000
-  // this serves both the API and the client
-  const PORT = 5000;
-  server.listen(PORT, "0.0.0.0", () => {
+  // Use PORT from environment variable or fallback to 5000
+  const PORT = parseInt(process.env.PORT || '5000', 10);
+  server.listen(PORT, () => {
     log(`serving on port ${PORT}`);
   });
 })();
