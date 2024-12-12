5875f7706e2573bd6c08047a711fb0d4b8bec9f9
Siar Alizadah <salizadah8@gmail.com>
2024-12-12 17:09:33 +0100

    second try3

--- app/vite.config.ts
+++ app/vite.config.ts
@@ -1,21 +1,13 @@
 import { defineConfig } from "vite";
 import react from "@vitejs/plugin-react";
-import themePlugin from "@replit/vite-plugin-shadcn-theme-json";
 import path from "path";
 import { fileURLToPath } from 'url';
-import checker from "vite-plugin-checker";
-import runtimeErrorOverlay from "@replit/vite-plugin-runtime-error-modal"
 
 const __filename = fileURLToPath(import.meta.url);
 const __dirname = path.dirname(__filename);
 
 export default defineConfig({
-  plugins: [
-    react(),
-    checker({ typescript: true, overlay: false }),
-    runtimeErrorOverlay(),
-    themePlugin(),
-  ],
+  plugins: [react()],
   resolve: {
     alias: {
       "@": path.resolve(__dirname, "client", "src"),
